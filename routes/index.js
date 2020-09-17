6b192e4277a2b67397fb442539310a4d4ceb92e5
cheong3 <soonyoung3a@gmail.com>
2020-09-18 01:43:25 +0900

    create login album navbar .ejs

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,16 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Final Test' });
 });
 
+router.get('/nav', function(req, res) {
+  res.render('nav');
+});
+
+router.get('/album', function(req, res) {
+  res.render('album');
+});
+
+router.get('/login', function(req, res) {
+  res.render('login');
+});
+
 module.exports = router;
