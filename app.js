c6d38d614504d2b5806935a1d0e8460ec0ab2c1a
LEEKYUHWAN <lgh971025@naver.com>
2020-09-17 16:39:32 +0900

    [Routing add]

--- app.js
+++ app.js
@@ -5,8 +5,11 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
+var loginRouter = require('./routes/login');
+var boardRouter = require('./routes/board');
+var albumRouter = require('./routes/album');
+var introRouter = require('./routes/intro');
+var memberRouter = require('./routes/member');
 var app = express();
 
 // view engine setup
@@ -20,7 +23,12 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/login', loginRouter);
+app.use('/board', boardRouter);
+app.use('/album', albumRouter);
+app.use('/intro', introRouter);
+app.use('/member', memberRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/intro.js
+++ routes/intro.js
@@ -1,9 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('인덱스입니다.');
+    res.render('intro');
 });
 
 module.exports = router;
