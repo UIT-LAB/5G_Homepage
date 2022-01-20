06a03d491d7f925ecb0afd977091e85086126b0a
chanyoung2371 <78065638+chanyoung2371@users.noreply.github.com>
2022-01-20 16:34:08 +0900

    Create Main

---
+++ client/src/component/Footer.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import '../style/Footer.css'
+
+function Footer() {
+  return (
+      <div>
+      <div className='footer'>
+          <ul className='footer_item'>
+              <li>개인정보 처리방침</li>
+              <li>우편번호 31499</li>
+              <li>주소: 충청남도 아산시 배방읍 호서로 79번길 20 제 2공학관 302호 ITRC 사업단</li>
+          </ul>
+      </div>
+      </div>
+  )
+}
+
+export default Footer;
