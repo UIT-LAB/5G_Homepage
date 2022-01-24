80d40800514037db4aafb63ddd066859dc0e7e78
Dong <ssuny303@gmail.com>
2022-01-24 16:37:36 +0900

    research CRUD, simple middleware for research

---
+++ middleware/test.js
@@ -0,0 +1,36 @@
+const url = require('url');
+const {get_writter} = require('../model/researchDAO');
+
+// const test = (req, res, next) => {
+//     const url = req.url;
+//     const slice = url.split('/');
+//     console.log(slice);
+//     console.log(req.url);
+//     next();
+// }
+
+const test = async (req, res, next) => {
+    if(req.body.jwtid == undefined || req.body.jwtname == undefined) {
+        res.send('<script>alert(`로그인이 되어있지 않습니다.`); location.href = `/login`</script>');
+    } else if(req.body.jwtid != undefined || req.body.jwtname != undefined){
+        const url = req.url;
+        const slice = url.split('/');
+        console.log(req.url);
+        let parameters = {
+            tid : req.params.num,
+            table : slice[1]
+        }
+        const writter = await get_writter(parameters);
+        console.log(writter);
+        if(req.body.jwtid == writter[0].writter) {
+            next();
+        } else {
+            // next();
+            res.send(`<script>alert('작성자만 수정 및 삭제가 가능합니다'); history.back()</script>`);
+        }
+    }
+}
+
+module.exports = {
+    test
+}
