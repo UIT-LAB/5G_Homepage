69ee26bed454e3fdeed045620f37e9ad23cb02fc
chanyoung <chanyoung2371@naver.com>
2022-02-12 14:53:35 +0900

    notice link edit

--- client/src/component/Image.jsx
+++ client/src/component/Image.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function Image() {
     return (
         <div>
-            <img style={{width:'100%',height:'500px'}}src='../image/5g_header.jpeg'></img>
+            <img style={{width:'100%',height:'100%'}}src='../image/header.jpg'></img>
         </div>
     )
 }
