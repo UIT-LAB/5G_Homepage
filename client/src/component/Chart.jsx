cbb337db349e826002028d175cb8a88d3e307bf8
chanyoung <chanyoung2371@naver.com>
2022-02-20 21:41:08 +0900

    Create Abot

--- client/src/component/Chart.jsx
+++ client/src/component/Chart.jsx
@@ -1,5 +1,12 @@
+import { display } from "@mui/system";
+import React from "react";
 function Chart() {
-  return <div style={{marginTop:'5%',textAlign:'center'}}> 조직도 입니다</div>;
+  return <div style={{marginTop:'5%',textAlign:'center' ,display:'flex' ,justifyContent:'center'}}>
+    <div style={{  width:'1000px' ,height:'800px'}}>
+      <img style={{width:'100%' , height:''}}src="../Image/business/조직도.jpg" alt="기다려주세요" />
+
+    </div>
+  </div>;
 }
 
 export default Chart;
